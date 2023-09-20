417e4d890a3bf9c2009fa71b703d50f36ce5905a
Gulbeyaz <glbzbayram@gmail.com>
2023-09-20 13:08:02 +0300

    Gerekli hook eklemesi App.js'e yapıldı

--- frontend/App.js
+++ frontend/App.js
@@ -3,22 +3,27 @@ import axios from "axios";
 
 import Charts from "./components/Charts";
 import Navbar from "./components/Navbar";
+import { useGeceModuAc } from "../hooks/useGeceModuAc";
+import useAxios from "../hooks/useAxios";
 
 const App = () => {
-  const [coinData, setCoinData] = useState([]);
-  const [geceModu, setGeceModu] = useState(false);
-
+  // const [coinData, setCoinData] = useState([]);
+  // const [geceModu, setGeceModu] = useState(false);
+  const [geceModu, savedTheme, toggleMode] = useGeceModuAc();
+  const [coinData, getCoins, coinsLoading, coinsErr] = useAxios(
+    "get",
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
+  );
   useEffect(() => {
-    axios
-      .get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
-      )
-      .then(res => setCoinData(res.data))
-      .catch(err => console.log(err));
+    getCoins();
   }, []);
   return (
     <div className={geceModu ? "dark-mode App" : "App"}>
-      <Navbar geceModu={geceModu} setGeceModu={setGeceModu} />
+      <Navbar
+        savedTheme={savedTheme}
+        geceModu={geceModu}
+        toggleMode={toggleMode}
+      />
       <Charts coinData={coinData} />
     </div>
   );
